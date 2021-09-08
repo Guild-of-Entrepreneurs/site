7c7a31372ace39a492b952fd796f0daa2f639c67
Alex Lin <alexlin@pop-os.localdomain>
2021-09-08 15:47:07 +1000

    working on responsive design in lead

--- src/components/App/Sections/Leading/Content.js
+++ src/components/App/Sections/Leading/Content.js
@@ -6,17 +6,18 @@ import Row from "../../../Layout/Row"
 
 const ContentStyles = styled(Row)`
   text-align: center;
+  margin-top: 5rem;
+  h3 {
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
 `
 
-const Content = (props) => (
+const Content = props => (
   <ContentStyles sd={2} ed={6} sm={3} em={9} sl={4} el={12}>
-    <h3>
-      {props.paragraphHeading}
-    </h3>
-    <p>
-      {props.paragraph}
-    </p>
+    <h3>{props.paragraphHeading}</h3>
+    <p>{props.paragraph}</p>
   </ContentStyles>
 )
 
-export default Content;
+export default Content
--- src/components/App/Sections/Leading/Heading.js
+++ src/components/App/Sections/Leading/Heading.js
@@ -6,14 +6,25 @@ import Row from "../../../Layout/Row"
 
 const HeadingStyles = styled(Row)`
   text-align: center;
+  h2 {
+    font-weight: 200;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  .highlight {
+    color: var(--primary);
+    font-weight: 700;
+  }
 `
 
 const Heading = () => (
-  <HeadingStyles sd={2} ed={6} sm={3} em={9} sl={4} el={12}>
+  <HeadingStyles sd={2} ed={6} sm={2} em={10} sl={4} el={12}>
     <h2>
-      If you want to be, are or support entrepreneurs, <span>The Guild is for you.</span>
+      If you want to be, are or support entrepreneurs,{" "}
+      <span className="highlight">The Guild is for you.</span>
     </h2>
   </HeadingStyles>
 )
 
-export default Heading;
+export default Heading
